c5dde94495c67a60af2237e94902d43b6b5aa949
DenisNovac <ponydnk@gmail.com>
2019-06-03 23:53:41 +0500

    reader is done, separate file for classes

--- gpt_reader.py
+++ gpt_reader.py
@@ -1,18 +1,27 @@
-# Program for searching partitions in GPT-disks.
+# Program for searching partitions in GPT-disks (for Python 3)
+# USAGE: sudo python3 gpt_reader.py DISK_PATH(/dev/sdX)
 # 
+# Other GPT utilities https://github.com/DenisNovac/GPTUtils
 # Documentation https://en.wikipedia.org/wiki/GUID_Partition_Table
 
 import sys
 
-LBA_SIZE=512
-GPT_ENTRY_SIZE=128
-DISK_SECTORS=0
+from gpt_structures import GptHeader
+from gpt_structures import GptPartition
 
+LBA_SIZE=512
+# GPT_ENTRY_SIZE=128 # 128 must be correct for most of the disks
+# Now it is calculated from header
+GPT_ENTRY_SIZE=128
+# size of the disk
+DISK_SECTORS=0
 # dictionary with detected partitions:
 # {partition unique guid:partition object}
 PARTITIONS_LIST=dict()
 
-# verifying gpt through signatures and Protective MBR partition
+
+
+# verifying gpt through signatures and Protective MBR test
 def verify_gpt( disk_path ):
     global DISK_SECTORS
     try:
@@ -29,64 +38,83 @@ def verify_gpt( disk_path ):
     else:
         print("MBR signature not found.")
         exit(-1)
-    
+
+    # reading Protective MBR
     partition_table = [ ]
     print("\nMBR Partition table: ")
     for i in range(0,4):
         partition_table.append(protective_mbr[0x01BE+16*i:0x01BE+16*i+16])
         print(partition_table[i].hex())
-    
+    # first partition must not be empty
     if partition_table[0].hex()=="0"*32:
         print("There is no first partition.")
         exit(-1)
-
+    # all the other partititons must be empty
     for i in range(1,4):
         if not partition_table[i].hex()=="0"*32:
             print("There is some MBR partition except first one. This is not a GPT disk.")
             exit(-1)
     
+    # the only record of protective mbr must ends at the end of disk
     guid_first_partition=partition_table[0]
     guid_first_partition_sectors=int.from_bytes(guid_first_partition[0x0C:0x0C+4],"little")+1
-    
     print("\nDisk size in sectors (if not - you are not running a GPT disk): "+str(guid_first_partition_sectors))
+
+    # the only record of protective mbr must have 0xEE signature
     gpt_signature=guid_first_partition[0x04:0x04+1]
     print("Signature of first MBR partition: "+gpt_signature.hex())
     if not gpt_signature.hex() == "ee":
         print("Signature is not 0xEE. This is not a GPT disk.")
         exit(-1)
     
+    # verifying complete
     print("GPT disk found on "+disk_path+".\n")
     DISK_SECTORS=guid_first_partition_sectors
 
-# there is two gpt tables on disk - at the beginning and end
 
+
+# there is two gpt tables on disk - at the beginning and end
 def read_primary_gpt_entries( disk_path ):
     file = open(disk_path,"rb")
     # jump to LBA 1
-    file.seek(LBA_SIZE) 
+    gpt_header_offset=LBA_SIZE
+    file.seek(gpt_header_offset) 
     gpt_header = file.read(LBA_SIZE)
     # read LBA 2-33
+    gpt_partition_table_offset=LBA_SIZE*2
     gpt_partition_table = file.read(LBA_SIZE*32)
     file.close()
-    print("GPT header: ")
-    print(gpt_header.hex())
+
+    header=GptHeader(gpt_header, gpt_header_offset, 
+                     gpt_partition_table, gpt_partition_table_offset)
+    header.print_info()
 
     get_partitions_list(gpt_partition_table, True)
-        
+
+
 
 def read_secondary_gpt_entries( disk_path ):
     file = open(disk_path,"rb")
     # jump to LBA -1
-    file.seek(DISK_SECTORS*512-LBA_SIZE)
+    secondary_gpt_header_offset=DISK_SECTORS*512-LBA_SIZE
+    file.seek(secondary_gpt_header_offset)
     secondary_gpt_header = file.read(LBA_SIZE)
-    print("\nSecondary GPT header: ")
-    print(secondary_gpt_header.hex())
     # jump to LBA -33, read LBA -33-(-2)
-    file.seek(DISK_SECTORS*512-LBA_SIZE*33)
+    secondary_gpt_partition_table_offset=DISK_SECTORS*512-LBA_SIZE*33
+    file.seek(secondary_gpt_partition_table_offset)
     secondary_gpt_partition_table=file.read(LBA_SIZE*32)
     file.close()
 
+
+    header=GptHeader(secondary_gpt_header, 
+                    secondary_gpt_header_offset, 
+                    secondary_gpt_partition_table, 
+                    secondary_gpt_partition_table_offset)
+    header.print_info()
+
     get_partitions_list(secondary_gpt_partition_table, False)
+
+
 
 # parse partition table into single entries, save to objects GPTPartition
 # and then save it in dictionary PARTITIONS_LIST as unique_guid:partition
@@ -115,50 +143,33 @@ def get_partitions_list ( raw_partition_table, is_primary ):
         # don't reverse second part
         guid.append(guid_last_part[0:2].hex())
         guid.append(guid_last_part[2:8].hex())
-
+        # join parts for standard style
         guid_string="-".join(guid)
         unique_guid=entry[16:32].hex()
-    
+
+        # if there is an actual partition
         if not unique_guid == "0"*32:
             partition=None
+            # check if it is inside PARTITIONS_LIST
             known_partition = PARTITIONS_LIST.get(unique_guid)
+            # if not - creating object
             if not known_partition:
-                partition=GPTPartition()
+                partition=GptPartition()
                 partition.guid=guid_string
                 partition.unique_guid=unique_guid
                 PARTITIONS_LIST.update({unique_guid:partition})
+            # if it is inside PARTITION_LIST - work with it
             if known_partition:
                 partition=known_partition
+            # update info about offsets and block
             if is_primary:
                 partition.primary_block=entry
                 partition.primary_offset=offset
             if not is_primary:
                 partition.secondary_block=entry
                 partition.secondary_offset=offset
-
         number=number+1
         offset=offset+GPT_ENTRY_SIZE
-
-# structure for saving detected GPT partitions
-class GPTPartition(object):
-    guid=None
-    unique_guid=None
-    primary_block=None
-    secondary_block=None
-    primary_offset=None
-    secondary_offset=None
-
-    def __init__(self):
-        pass
-
-    def info(self):
-        
-        print("\n-------------"+self.guid+" : "+self.unique_guid+"-------------")
-        print("\n"+str(self.primary_offset)+":")
-        print(self.primary_block.hex())
-
-        print("\n"+str(self.secondary_offset)+":")
-        print(self.secondary_block.hex())
 
 
 
@@ -168,15 +179,15 @@ def main( args ):
         read_primary_gpt_entries(args[1])
         read_secondary_gpt_entries(args[1])
     except IndexError:
-        print("USAGE: python3 GPTreader.py DISK_PATH(/dev/sdX)")
+        print("USAGE: sudo python3 gpt_reader.py DISK_PATH(/dev/sdX)")
         exit(-1)
-    
+    print("\n\nDetected partitions: "+str(len(PARTITIONS_LIST)))
     for k in PARTITIONS_LIST.keys():
         partition=PARTITIONS_LIST.get(k)
         partition.info()
-
-    print(len(PARTITIONS_LIST))
-        
-
-
-main( sys.argv )
+    # return for other scripts in repo
+    return PARTITIONS_LIST
+    
+# need this if execution is not from import
+if __name__ == "__main__":
+    main(sys.argv)
---
+++ gpt_structures.py
@@ -0,0 +1,98 @@
+# Structures for searching partitions in GPT-disks (for Python 3)
+# USAGE: sudo python3 gpt_reader.py DISK_PATH(/dev/sdX)
+# 
+# Other GPT utilities https://github.com/DenisNovac/GPTUtils
+# Documentation https://en.wikipedia.org/wiki/GUID_Partition_Table
+
+import zlib
+
+# structure for saving detected GPT partitions
+class GptPartition( object ):
+    guid=None
+    unique_guid=None
+    primary_block=None
+    secondary_block=None
+    primary_offset=None
+    secondary_offset=None
+
+    def __init__( self ):
+        pass
+
+    def info( self ):
+        print("\n-------------"+self.guid+" : "+self.unique_guid+"-------------")
+        print("\n"+str(self.primary_offset)+":")
+        print(self.primary_block.hex())
+        print("\n"+str(self.secondary_offset)+":")
+        print(self.secondary_block.hex())
+
+
+
+# structure for work with gpt headers
+class GptHeader( object ):
+    gpt_header=None
+    header_size=None
+    entry_size=None
+    checksum=None
+    current_lba=None
+    backup_lba=None
+    entries_number=None
+    entries_checksum=None
+
+    gpt_header_offset=None
+    partition_table=None
+    partition_table_offset=None
+
+    def __init__( self, gpt_header, gpt_header_offset, partition_table, partition_table_offset ):
+        self.gpt_header=gpt_header
+        self.header_size=int.from_bytes(gpt_header[0x0C:0x0C+4],"little")
+        self.entry_size=int.from_bytes(gpt_header[0x54:0x54+4],"little")
+        self.checksum=self.gpt_header[0x10:0x10+4]
+        self.current_lba=int.from_bytes(gpt_header[0x18:0x18+8],"little")
+        self.backup_lba=int.from_bytes(gpt_header[0x20:0x20+8],"little")
+        self.entries_number=int.from_bytes(gpt_header[0x50:0x50+4],"little")
+        self.entries_checksum=self.gpt_header[0x58:0x58+4]
+
+        self.gpt_header_offset=gpt_header_offset
+        self.partition_table=partition_table
+        self.partition_table_offset=partition_table_offset
+        pass
+
+    def calculate_checksum( self ):
+        # creation of gpt_header with zeroed checksum field
+        gpt_header_zero_cs=[ ]
+        gpt_header_zero_cs.append(self.gpt_header[0:0x10])
+        gpt_header_zero_cs.append(bytes(bytearray(4)))
+        gpt_header_zero_cs.append(self.gpt_header[0x14:self.header_size])
+        gpt_header_zero_cs=b''.join(gpt_header_zero_cs)
+        # checksum in big-endian
+        checksum=zlib.crc32(gpt_header_zero_cs)
+        # checksum in little-endian bytes
+        checksum=checksum.to_bytes((checksum.bit_length()+7)//8,"little")
+        return checksum
+
+    # partition table CRC32/zlib checksum
+    def calculate_partition_table_checksum( self, partition_table ):
+        # partition table checksum in big-endian
+        checksum=zlib.crc32(partition_table)
+        # patition table checksum in little-endian bytes
+        checksum=checksum.to_bytes((checksum.bit_length()+7)//8,"little")
+        # return in bytes
+        return checksum
+
+    def print_info( self ):
+        print("\n\nGPT header ("+str(self.gpt_header_offset)+"): ")
+        print(self.gpt_header.hex())
+        print("Header size: "+str(self.header_size))
+        print("Checksum: "+self.checksum.hex())
+        print("Current LBA: "+str(self.current_lba))
+        print("Backup LBA: "+str(self.backup_lba))
+        print("Entries number: "+str(self.entries_number))
+        print("Entry size: "+str(self.entry_size))
+        print("Partition table offset: "+str(self.partition_table_offset))
+        print("Partition table checksum: "+self.entries_checksum.hex())
+
+        print("\nCalculated header checksum: "+self.calculate_checksum().hex())
+        print("Calculated partition table checksum: "+
+            self.calculate_partition_table_checksum(self.partition_table).hex())
+
+    
